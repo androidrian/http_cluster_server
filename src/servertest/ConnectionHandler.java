f28f60194da9bcce22d1936584041fd3a7b0d834
1100427- Joao M. <1100427@isep.ipp.pt>
2016-05-18 01:36:57 +0100

    correction to html parser (index creatin upon client call)

--- src/servertest/ConnectionHandler.java
+++ src/servertest/ConnectionHandler.java
@@ -42,52 +42,50 @@ public class ConnectionHandler extends Thread { //ao extender para Thread, esta 
     public void run() {
 
         while (true) {
-           try {
-               s = null;
-
-            
+            try {
+                s = null;
 
                 this.s = serverSocket.accept();
 
                 br = new BufferedReader(new InputStreamReader(s.getInputStream()));
                 pw = new PrintWriter(s.getOutputStream());
-            } catch (IOException ex) {
-                Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
 
-            hp.printDirectoryListFilenames(filelist);
-            try {
-                hp.buildHTML(filelist);
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            try {
-                //aqui fazemos o get do request e passamos a string para o HTTPRequest
-                String sReq = "";
-
+                hp.printDirectoryListFilenames(filelist);
                 try {
-                //do br temos que ler o request
-                    // ler até o request não chegar ou até br estar pronto
-                    while (br.ready() || sReq.length() == 0) {
-                        sReq += (char) br.read();
-                    }
-                    System.out.println(sReq); //para mostrar o request
-                } catch (IOException ex) {
+                    hp.buildHTML(filelist);
+                } catch (FileNotFoundException ex) {
                     Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                //criamos o request
-                HTTPRequest req = new HTTPRequest(sReq);
+                try {
+                    //aqui fazemos o get do request e passamos a string para o HTTPRequest
+                    String sReq = "";
 
-                //passamos o request para o response para ter a resposta
-                HTTPResponse res = new HTTPResponse(req);
-                //write o output final para o pw
-                pw.write(res.response.toCharArray());
-                //pofechar por esta ordem
-                pw.close();
-                br.close();
-                s.close();//o sockect é o último a ser fechado
-            } catch (Exception e) {
-                e.printStackTrace();
+                    try {
+                //do br temos que ler o request
+                        // ler até o request não chegar ou até br estar pronto
+                        while (br.ready() || sReq.length() == 0) {
+                            sReq += (char) br.read();
+                        }
+                        System.out.println(sReq); //para mostrar o request
+                    } catch (IOException ex) {
+                        Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    //criamos o request
+                    HTTPRequest req = new HTTPRequest(sReq);
+
+                    //passamos o request para o response para ter a resposta
+                    HTTPResponse res = new HTTPResponse(req);
+                    //write o output final para o pw
+                    pw.write(res.response.toCharArray());
+                    //pofechar por esta ordem
+                    pw.close();
+                    br.close();
+                    s.close();//o sockect é o último a ser fechado
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
